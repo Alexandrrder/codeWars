90f215cce493fd5302a20f107b23110f407431a4
alexandrrder <alexandrrder@gmail.com>
2025-01-10 13:54:46 +0100

    first commit

--- 8kyu/array.js
+++ 8kyu/array.js
@@ -1,5 +1,6 @@
 //Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 //Return your answer as a number.
-function sumMix(arr) {
+/*/ function sumMix(arr) {
     return arr.reduce((sum, current) => sum + Number(current), 0);
   }
+/*/
---
+++ 8kyu/function.js
@@ -0,0 +1,7 @@
+function multiply(a, b){
+    let result;
+    result = a * b;
+    return result;
+  }
+console.log(multiply(2,2));
+  
